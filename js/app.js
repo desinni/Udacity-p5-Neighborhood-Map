4a0b7e58ef21165258419f80895729e6a0a1a4b9
desinni <laura.zumbakyte@gmail.com>
2016-06-01 16:24:11 +0300

    Add info windows to markers

--- js/app.js
+++ js/app.js
@@ -4,27 +4,32 @@ var initialPlaces = [
   {
     title: "The British Museum",
     lat: 51.519360,
-    lng: -0.127038
+    lng: -0.127038,
+    placeId: "ChIJB9OTMDIbdkgRp0JWbQGZsS8"
   },
   {
     title: "London Eye",
     lat: 51.503333,
-    lng: -0.119571
+    lng: -0.119571,
+    placeId: "ChIJc2nSALkEdkgRkuoJJBfzkUI"
   },
   {
     title: "Buckingham Palace",
     lat: 51.501337,
-    lng: -0.141271
+    lng: -0.141271,
+    placeId: "ChIJtV5bzSAFdkgRpwLZFPWrJgo"
   },
   {
     title: "Madame Tussauds London",
     lat: 51.522802,
-    lng: -0.154962
+    lng: -0.154962,
+    placeId: "ChIJgZ24Us4adkgRpDNAwNPO_SY"
   },
   {
     title: "Big Ben",
     lat: 51.500722,
-    lng: -0.124379
+    lng: -0.124379,
+    placeId: "ChIJ2dGMjMMEdkgRqVqkuXQkj7c"
   }
 ];
 
